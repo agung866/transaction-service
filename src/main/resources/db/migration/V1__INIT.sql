a3876c19bc92a461d8d4a7acb36edbb424e89397
agung866 <setiawan.agung866@hmail.com>
2025-04-19 19:06:07 +0700

    update code

--- src/main/resources/db/migration/V1__INIT.sql
+++ src/main/resources/db/migration/V1__INIT.sql
@@ -1,25 +1,12 @@
-CREATE TABLE schema_product.product (
-    id SERIAL PRIMARY KEY,
-    transaction_id VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL,
-    total_price NUMERIC NOT NULL,
-    status VARCHAR(50),
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    stock INT,
+    price NUMERIC ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by VARCHAR(255) DEFAULT 'system',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_by VARCHAR(255) DEFAULT 'system'
-);
-
-CREATE TABLE schema_transaction."trx_item" (
-    id SERIAL PRIMARY KEY,
-    transaction_id VARCHAR(255) NOT NULL,
-    product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    price NUMERIC NOT NULL,
-    total_price NUMERIC NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    created_by VARCHAR(255) DEFAULT 'system',
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_by VARCHAR(255) DEFAULT 'system',
-    CONSTRAINT fk_transaction FOREIGN KEY(transaction_id) REFERENCES schema_transaction."transaction"(transaction_id) ON DELETE CASCADE
 );
