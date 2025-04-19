f314d30ef79a7cda2b4bdfb7dcf580f442d96269
agung866 <setiawan.agung866@hmail.com>
2025-04-19 23:04:46 +0700

    update code

--- src/main/java/com/example/demo/events/SuccessPaymentEvent.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.events;
-
-import io.soabase.recordbuilder.core.RecordBuilder;
-
-@RecordBuilder
-public record SuccessPaymentEvent(Long orderId,
-                                  Long productId, int qty){
-}
--- src/main/java/com/example/demo/service/UpdateStockService.java
+++ src/main/java/com/example/demo/service/UpdateStockService.java
@@ -1,9 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.events.SuccessPaymentEvent;
+import com.example.demo.errorhandling.DataNotFoundException;
 import com.example.demo.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.common.events.SuccessPaymentEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +17,14 @@ public class UpdateStockService {
 
     @KafkaListener(topics = "${spring.kafka.topic.payment-success}", groupId = "${spring.kafka.consumer.group-id}")
     public void execute(SuccessPaymentEvent event) {
-        log.info("Mengupdate stok untuk order: {}", event.orderId());
-
-        productRepository.findByProductId(event.productId()).map(
-                product -> {
-                    product.setStock(product.getStock() - event.qty());
-                    return productRepository.save(product);
-                }
-        );
+        log.info("Mengupdate stok untuk order: {}", event.transactionId());
+        event.items().forEach(itemDetail -> {
+            productRepository.findByProductId(itemDetail.productId()).ifPresent(product -> {
+                product.setStock(product.getStock() - itemDetail.quantity());
+                productRepository.save(product);
+                log.info("Stok produk {} berhasil diperbarui", product.getProductId());
+            });
+        });
     }
 }
 
